997068e2542a1f6aac7593d00dd05b7b97bc9f63
aungsoeoo <aungsoeoo.94s@gmail.com>
2020-02-12 01:36:26 +0630

    first commit

---
+++ app/src/main/java/com/oxoo/spagreen/Config.java
@@ -0,0 +1,37 @@
+package com.oxoo.spagreen;
+
+/**
+ * OXOO - Android Live TV & Movie Portal App
+ * ---------------------- OXOO --------------------
+ * --------------------- V1.1.4 -------------------
+ * ------- Android Live TV & Movie Portal App --------
+ *
+ * @package     OXOO - Android Live TV & Movie Portal App
+ * @author      Abdul Mannan/Spa Green Creative
+ * @copyright   Copyright (c) 2014 - 2019 SpaGreen,
+ * @license     http://codecanyon.net/wiki/support/legal-terms/licensing-terms/
+ * @profile     https://codecanyon.net/item/oxoo-android-live-tv-movie-portal-app-with-powerful-admin-panel/23526581
+ * @link        http://www.spagreen.net
+ * @link        support@spagreen.net
+ *
+ **/
+
+public class Config {
+
+    // copy your api url from php admin dashboard & paste below
+    public static final String API_SERVER_URL = "https://app.burmesesubtitles.com/api/";
+
+    //copy your api key from php admin dashboard & paste below
+    public static final String API_KEY = "p8a8o42diyrn4iq0fwi7vqda";
+
+    //copy your terms url from php admin dashboard & paste below
+    public static final String TERMS_URL = "https://app.burmesesubtitles.com/terms/";
+
+    //default theme
+    public static boolean DEFAULT_DARK_THEME_ENABLE = true;
+
+    // youtube video autoplay
+    // Note: Google/Youtube does not recommended autoplay
+    public static boolean YOUTUBE_AUTOPLAY = true;
+
+}
